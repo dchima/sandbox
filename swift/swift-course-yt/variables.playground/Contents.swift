ea5b6436bc847007b8c6e0de5092c6c691c7a528
dchima <dcnnad@gmail.com>
2023-05-01 21:25:10 +0100

    swift work

--- swift/swift-course-yt/variables.playground/Contents.swift
+++ swift/swift-course-yt/variables.playground/Contents.swift
@@ -1,1 +1,13 @@
+import Foundation
 
+
+//let -> kinda like const in js
+let myName = "chima"
+
+//var -> kinda like let in js (hilarious)
+var yourName = "Foo"
+
+var names = [ myName, yourName ]
+
+names.append("Bar")
+names.append("Baz")
